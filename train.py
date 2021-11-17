321fdc84912b8d962efc974b655d393eb2200c64
AlessioGalluccio <alessio.galluccio@mail.polimi.it>
2021-11-17 15:55:04 +0100

    Comment neptune

--- train.py
+++ train.py
@@ -8,6 +8,7 @@ from localization import export_gradient_maps
 from model import DifferNet, save_model, save_weights
 from utils import *
 
+'''
 import neptune.new as neptune
 import config as c
 
@@ -20,6 +21,7 @@ run["name_dataset"] = [c.dataset_path]
 run["img_dims"] = [c.img_dims]
 run["device"] = c.device
 run["n_scales"] = c.n_scales
+'''
 
 
 class Score_Observer:
@@ -105,8 +107,10 @@ def train(train_loader, test_loader):
                          print_score=c.verbose or epoch == c.meta_epochs - 1)
         score_obs_aucpr.update(average_precision_score(is_anomaly, anomaly_score), epoch,
                          print_score=c.verbose or epoch == c.meta_epochs - 1)
+        '''
         run["train/auroc"].log(score_obs_auroc.last)
         run["train/aucpr"].log(score_obs_aucpr.last)
+        '''
 
 
 
